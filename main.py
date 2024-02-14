984e98f0c99e23309493bbd66ca7fea3afa910b3
pjsalisbury <patrickjsalisbury@gmail.com>
2024-02-14 00:08:51 -0500

    config file added and several code changes made

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# testing script to use to text xpublish_wms. For now, only the nc datasets are supported.
-
 import requests
 import numpy as np
 import mercantile
@@ -7,102 +5,147 @@ import random
 import threading
 import concurrent.futures
 import time
+import tomllib
 
-# disable or enable a multi-threaded testing run (true/false). if the script is running in multi-threaded mode,
-# threads_per_layer specifies how many threads are created for each data layer
-multi_threading = "false"
-
+# globals for config file values initialized to placeholder/empty values
+multi_threading = False
 threads_per_layer = 1
-
-hostname = "http://localhost:9000"
-
-# file names for the single-threaded and multi-threaded modes respectively
-single_filename = ""
-threaded_filename = ""
-
-# the tile width and height used for the generated testing tiles. For now we will leave this at 256x256 since this is
-# what ncWMS uses by default
-tilewidth = "256"
-tileheight = "256"
-
-# specify the zoom range to use during random tile generation. note that zoom levels below 4 will not be constrained
-# in latitude to the actual dataset, so for the most accurate results the minzoom should be 4 or higher
+hostname = ""
+filename = ""
+tilewidth = 256
+tileheight = 256
 minzoom = 4
 maxzoom = 8
-
-# specify the number of trials to perform (per layer/thread). note that this will take a very long time with high
-# trial numbers
-numTrials = 50
-
-# specify the testing layers here. these are the four layers we used in the previous testing
-#layers = ["test1%2FWind_speed_surface",
-#          "test1%2FMean_period_of_wind_waves_surface",
-#          "test2%2Fssh",
-#          "test2%2Fsurface_boundary_layer_thickness"]
-
-# layers = ["wd",
-#           "tmp2m"]
-
-layers = ["ssh",
-          "surface_boundary_layer_thickness"]
-
-
+num_trials = 1
+layers = []
+epsg = 3857
+limit_x = False
+limit_y = False
+min_x = -1
+min_y = -1
+max_x = -1
+max_y = -1
 
 # lock to allow for thread-safe file and stdout write
 write_lock = threading.Lock()
 
 
-# generates a random tile for a given layer (using a random tile and zoom level selected from the spherical mercator system)
-# returns the request URL to get that tile
-def generateTile(layerName):
-    # IMPORTANT: Our grib2 dataset only covers from latitude +52 to latitude -15. As a result, we will exclude generating
-    # tiles that would fall completely outside this latitude range during our testing.
+# reads in the config file and populates the globals
+def readconfig():
+    global hostname
+    global filename
+    global multi_threading
+    global threads_per_layer
+    global num_trials
+    global epsg
+    global tilewidth
+    global tileheight
+    global minzoom
+    global maxzoom
+    global limit_y
+    global limit_x
+    global min_x
+    global min_y
+    global max_x
+    global max_y
 
-    # generate a random zoom level (for this testing we will limit zoom to between zoom 4 and zoom 8 because higher zoom
-    # levels are unrealistic with our data resolution)
+    with open("config.toml", "rb") as f:
+        data = tomllib.load(f)
+
+        server_settings = data["server_settings"]
+        layers_obj = data["layers"]
+        script_settings = data["script_settings"]
+        tile_settings = data["tile_settings"]
+        advanced = data["advanced"]
+
+        hostname = server_settings["hostname"]
+        for key, value in layers_obj.items():
+            if value != '':
+                layers.append(value)
+
+        filename = script_settings["filename"]
+        multi_threading = script_settings["multi_threading"]
+        threads_per_layer = script_settings["threads_per_layer"]
+        num_trials = script_settings["num_trials"]
+        epsg = script_settings["epsg"]
+
+        tilewidth = tile_settings["tile_width"]
+        tileheight = tile_settings["tile_height"]
+        minzoom = tile_settings["min_zoom"]
+        maxzoom = tile_settings["max_zoom"]
+
+        limit_x = advanced["limit_x"]
+        limit_y = advanced["limit_y"]
+        min_x = advanced["min_x"]
+        min_y = advanced["min_y"]
+        max_x = advanced["max_x"]
+        max_y = advanced["max_y"]
+
+
+# randomly generates the query dict for a specific request
+def generatequery(layer_name):
+
+    # generate a random zoom level
     zoomlvl = random.randint(minzoom, maxzoom)
 
-    # generate random tile number n for x (lon). here we will take the zoom level squared as n+1 and select from 0-n
-    tile_x = random.randint(0, (2 ** zoomlvl) - 1)
-
-    if zoomlvl > 3:
-        # generate random tile number n for y (lat). This is more complex because we need to restrict to our latitude limits.
-        # for zoom level 4, we want to limit to tiles 5-8. for zoom level 5, this would turn into tiles 10-16, and so on.
-        # as a result, we will multiply 5 and 8 by 2^n to get our limits, where n is the number of zoom levels above 4.
-        tile_y = random.randint(5 * (2 ** (zoomlvl - 4)), 8 * (2 ** (zoomlvl - 4)))
+    # generate x tile
+    if limit_x:
+        tile_x = random.randint(min_x * (2 ** (zoomlvl - minzoom)), max_x * (2 ** (zoomlvl - minzoom)))
     else:
-        # if the zoom level happens to be under 4, ignore the above rules
+        tile_x = random.randint(0, (2 ** zoomlvl) - 1)
+
+    # generate y tile
+    if limit_y:
+        tile_y = random.randint(min_y * (2 ** (zoomlvl - minzoom)), max_y * (2 ** (zoomlvl - minzoom)))
+    else:
         tile_y = random.randint(0, (2 ** zoomlvl) - 1)
 
 
     # generate the bounding box using mercantile
-    boundingbox = mercantile.bounds(tile_x, tile_y, zoomlvl)
+    boundingboxstr = ''
+    if epsg == 3857:
+        boundingbox = mercantile.xy_bounds(tile_x, tile_y, zoomlvl)
+        boundingboxstr = (str(boundingbox.left) + ',' + str(boundingbox.bottom) + ',' +
+                          str(boundingbox.right) + ',' + str(boundingbox.top))
+    else: # catch-all else in case someone messes up specifying 4326
+        boundingbox = mercantile.bounds(tile_x, tile_y, zoomlvl)
 
-    # convert to string for url (format is west,south,east,north)
-    boundingboxstr = (str(boundingbox.west) + ',' + str(boundingbox.south) + ',' +
-                      str(boundingbox.east) + ',' + str(boundingbox.north))
-    # this is the 'template' request url that the randomTile function will complete to generate a random tile
-    # the hostname, layer name, and bounding box are added dynamically for each request
-    # the 'TIME' parameter is omitted from the URL since the datasets we are testing are valid only at a single time
-    tileurl = (hostname + "/wms/?FORMAT=image%2Fpng&TRANSPARENT=TRUE&STYLES=default-scalar%2Fdefault&LAYERS=" +
-                layerName + "&COLORSCALERANGE=-1.082%2C24.48&NUMCOLORBANDS=250&ABOVEMAXCOLOR=0x000000" +
-                            "&BELOWMINCOLOR=0x000000&BGCOLOR=transparent&LOGSCALE=false&SERVICE=WMS&VERSION=1.1.1" +
-                            "&REQUEST=GetMap&SRS=EPSG%3A3857&BBOX=" +
-                boundingboxstr + "&WIDTH=" + tilewidth + "&HEIGHT=" + tileheight)
+        # convert to string for url (format is west,south,east,north)
+        boundingboxstr = (str(boundingbox.west) + ',' + str(boundingbox.south) + ',' +
+                          str(boundingbox.east) + ',' + str(boundingbox.north))
 
-    return tileurl
+    # the wms query. TODO a lot here is hard coded that should be changed
+    query = {
+        'SERVICE': 'WMS',
+        'VERSION': '1.3.0',
+        'REQUEST': 'GetMap',
+        'LAYERS': layer_name,
+        'CRS': 'EPSG:' + str(epsg),
+        'STYLES': 'raster/default',
+        'WIDTH': tilewidth,
+        'HEIGHT': tileheight,
+        'BBOX': boundingboxstr,
+        'COLORSCALERANGE': '0.957,1200', # worst hard-code offender cause a lot of the tiles will look garbage
+    }
 
-def runBatchTest(layerName, fileToWrite):
+    return query
+
+
+def runbatchtest(layer_name, file_to_write):
 
     # numpy array of trial results. This allows us to calculate more complicated statistics on the trial results
     trials = np.array([])
 
-    for x in range(numTrials):
-        printout = "Batch testing layer " + layerName + ": " + str(x+1) + "/" + str(numTrials)
+    for x in range(num_trials):
+        printout = "Batch testing layer " + layer_name + ": " + str(x + 1) + "/" + str(num_trials)
         print("\r" + printout, end="")
         start = time.perf_counter()
-        response = requests.get(url=generateTile(layerName))
-        trials = np.append(trials, time.perf_counter() - start)
+        response = requests.get(url=hostname, params=generatequery(layer_name))
+        # check the code to ensure its 200 otherwise flood the user's terminal in a very bad way (for now)
+        if response.status_code == 200:
+            trials = np.append(trials, time.perf_counter() - start)
+        else:
+            print("error with layer " + layer_name)
 
     # calculate some statistics on our results
     avgtime = round((np.average(trials) * 1000), 4)
@@ -113,21 +156,25 @@ def runBatchTest(layerName, fileToWrite):
     totaltime = round(sum(trials) * 1000, 4)
 
     print("")
-    fileToWrite.write(layerName + "\t" + format(totaltime) + "\t" + format(avgtime) + "\t" + format(lowerquart) + "\t" +
-                      format(upperquart) + "\t" + format(onepercentlow) + "\t" + format(onepercenthigh) + "\n")
+    file_to_write.write(layer_name + "\t" + format(totaltime) + "\t" + format(avgtime) + "\t" + format(lowerquart) + "\t" +
+                        format(upperquart) + "\t" + format(onepercentlow) + "\t" + format(onepercenthigh) + "\n")
 
 # run batch tests with multiple threads
-def testingThread(layerName, fileToWrite):
+def testing_thread(layer_name, fileToWrite):
 
     # numpy array of trial results. This allows us to calculate more complicated statistics on the trial results
     trials = np.array([])
     with write_lock:
-        print("Thread testing " + layerName + " started")
+        print("Thread testing " + layer_name + " started")
 
-    for x in range(numTrials):
+    for x in range(num_trials):
         start = time.perf_counter()
-        response = requests.get(url=generateTile(layerName))
-        trials = np.append(trials, time.perf_counter() - start)
+        response = requests.get(url=hostname, params=generatequery(layer_name))
+        if response.status_code == 200:
+            trials = np.append(trials, time.perf_counter() - start)
+        else:
+            with write_lock:
+                print("error with layer " + layer_name)
 
     # calculate some statistics on our results
     avgtime = round((np.average(trials) * 1000), 4)
@@ -138,19 +185,23 @@ def testingThread(layerName, fileToWrite):
     totaltime = round(sum(trials) * 1000, 4)
 
     with write_lock:
-        print("Thread testing " + layerName + " finished")
+        print("Thread testing " + layer_name + " finished")
         with open(fileToWrite, 'a') as file:
-            file.write(layerName + "\t" + format(totaltime) + "\t" + format(avgtime) + "\t" + format(lowerquart) +
+            file.write(layer_name + "\t" + format(totaltime) + "\t" + format(avgtime) + "\t" + format(lowerquart) +
                        "\t" + format(upperquart) + "\t" + format(onepercentlow) + "\t" + format(onepercenthigh) + "\n")
 
 
 if __name__ == "__main__":
+
+    # populate the globals
+    readconfig()
+
     if multi_threading == "true":
         print("script running in multi-threaded mode\n")
 
         # clear file and add file headers
-        with open(threaded_filename, 'w') as f:
-            f.write("Layer\tTotal time(ms)\tAvg time (ms)\tLower quartile (ms)\tUpper quartile (ms)\tLower 1% (ms)\tUpper 1% (ms)\n")
+        with open(filename, 'w') as f:
+            f.write("Layer on thread\tTotal thread execution time(ms)\tAvg request time (ms)\tLower quartile (ms)\tUpper quartile (ms)\tLower 1% (ms)\tUpper 1% (ms)\n")
 
         # create iterable of layers to pass to threads
         layersforthreads = []
@@ -160,20 +211,20 @@ if __name__ == "__main__":
         # create iterable of output file name
         filenameforthreads = []
         for x in range(len(layersforthreads)):
-            filenameforthreads.append(threaded_filename)
+            filenameforthreads.append(filename)
 
         # specify number of threads
         thread_num = len(layersforthreads)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=thread_num) as executor:
-            executor.map(testingThread, layersforthreads, filenameforthreads)
+            executor.map(testing_thread, layersforthreads, filenameforthreads)
 
     else:
         print("script running in single-threaded mode\n")
-        with open(single_filename, 'w') as f:
-            f.write("Layer\tTotal time(ms)\tAvg time (ms)\tLower quartile (ms)\tUpper quartile (ms)\tLower 1% (ms)\tUpper 1% (ms)\n")
+        with open(filename, 'w') as f:
+            f.write("Layer\tTotal time(ms)\tAvg request time (ms)\tLower quartile (ms)\tUpper quartile (ms)\tLower 1% (ms)\tUpper 1% (ms)\n")
 
             for layer in layers:
-                runBatchTest(layer, f)
+                runbatchtest(layer, f)
 
             print("Saving average file")
